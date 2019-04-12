ab38e66c4adbfeeaf81600777df0ea703797f60e
dilap54 <dilap54@mail.ru>
2019-04-12 20:29:23 +0300

    Добавил некоторые уведомления

--- src/reducers/alerts.js
+++ src/reducers/alerts.js
@@ -1,6 +1,18 @@
 import omit from 'lodash/omit'
 
 import { DELETE_ALERT } from '../actions/alerts'
+import {
+  AUTH_FAILED,
+  AUTH_SUCCESS,
+} from '../actions/auth'
+import {
+  FILE_UPLOAD_SUCCESSFUL,
+  FILE_UPLOAD_FAILED,
+  PRODUCT_CREATE_SUCCESSFUL,
+  PRODUCT_CREATE_FAILED,
+  UPDATE_PRODUCT_SUCCESSFUL,
+  UPDATE_PRODUCT_FAILED
+} from '../actions/products'
 
 const initialState = {
   byTimeStamp: {}
@@ -25,7 +37,15 @@ function addAlertReducer(state, variant, text, closeAfter){
 function alertsReducer(state = initialState, action) {
   switch (action.type) {
   case DELETE_ALERT:
-    return {...state, byTimeStamp: omit(state.byTimeStamp, [action.id])}   
+    return {...state, byTimeStamp: omit(state.byTimeStamp, [action.id])}
+  case AUTH_SUCCESS: return addAlertReducer(state, 'success', 'Авторизован на сервере', 500)
+  case AUTH_FAILED: return addAlertReducer(state, 'danger', 'Ошибка авторизации', 1000)
+  case FILE_UPLOAD_SUCCESSFUL: return addAlertReducer(state, 'success', 'Файл загружен', 1000)
+  case FILE_UPLOAD_FAILED: return addAlertReducer(state, 'danger', 'Ошибка загрузки файла', 1000)
+  case PRODUCT_CREATE_SUCCESSFUL: return addAlertReducer(state, 'success', 'Изделие создано', 1000)
+  case PRODUCT_CREATE_FAILED: return addAlertReducer(state, 'danger', 'Ошибка создания изделия', 1000)
+  case UPDATE_PRODUCT_SUCCESSFUL: return addAlertReducer(state, 'success', 'Изделие обновлено', 1000)
+  case UPDATE_PRODUCT_FAILED: return addAlertReducer(state, 'danger', 'Ошибка обновления изделия', 1000)
   default:
     return state
   }
