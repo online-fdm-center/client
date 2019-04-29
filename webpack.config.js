b9b017088a9cbe300f0d1ab6444b3d83c7906dfc
dilap54 <dilap54@mail.ru>
2019-04-29 21:57:45 +0300

    Взятие API_URL из переменных окружения во время сборки

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
       template: "./src/index.html"
     }),
     new webpack.DefinePlugin({
-      'API_URL': JSON.stringify('api.online.fdm.center')
+      'API_URL': JSON.stringify(process.env.API_URL || 'api.online.fdm.center')
     })
   ]
 };
